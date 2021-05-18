8920584dc1465d1eb399b1d7cf8545bac50a73b3
WhoIsVladimir <bibubl@yandex.com>
2021-05-18 13:59:56 +0300

    Залив в git

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float timeFix = 0f;
+    float timer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = Time.time - timeFix;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& timer > 2f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeFix = Time.time;
+            Debug.Log(timer.ToString());
+        }
+    }
+
+
+}
